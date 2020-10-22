52cd54dd786ea25aab4d330cedf098ca6969ef1d
monicaleep <monica.parrillo@gmail.com>
2020-10-21 20:37:10 -0400

    get MVP done for Part 1

--- index.js
+++ index.js
@@ -3,42 +3,58 @@ const express = require('express');
 const app = express();
 const ejsLayouts = require('express-ejs-layouts');
 const axios = require('axios')
-//const methodOverride = require('method-override');
 
 // set view engine to use ejs
 app.set('view engine', 'ejs');
 app.use(ejsLayouts);
+
 // body parser setup to get data from forms
 app.use(express.urlencoded({
   extended: false
 }));
 
-app.get('/',(req,res)=>{
+
+
+app.get('/', (req, res) => {
   res.render('home')
 })
 
 // read all movies
-app.get('/movies',(req,res)=>{
+app.get('/movies', (req, res) => {
   const searchQuery = req.query.q;
-  res.render('results')
+  axios.get(`http://www.omdbapi.com/?apikey=${process.env.API_KEY}&s='${searchQuery}'`)
+    .then(response => {
+      res.render('results',{data:response.data})
+    })
+    .catch(err => {
+      console.log(err)
+    })
 })
 
+
+
 // show details about 1 movie
-app.get('/movies/:id',(req,res)=>{
-  let idx = req.params.id;
-  res.render('show')
+app.get('/movies/:id', (req, res) => {
+  let id = req.params.id;
+
+  axios.get(`http://www.omdbapi.com/?i=${id}&apikey=${process.env.API_KEY}`)
+    .then(response=>{
+      console.log(response.data)
+      res.render('show', {data: response.data})
+    })
+    .catch(err=>console.log(err))
 })
 
 // create a fave movie
-app.post('/faves',(req,res)=>{
+app.post('/faves', (req, res) => {
 
 })
 
 // read all fave movies
-app.get('/faves',(req,res)=>{
+app.get('/faves', (req, res) => {
   res.render('faves')
 })
 
-app.listen(process.env.PORT || 3000,()=>{
+app.listen(process.env.PORT || 3000, () => {
   console.log(`listening on port ${process.env.PORT}`)
 })
